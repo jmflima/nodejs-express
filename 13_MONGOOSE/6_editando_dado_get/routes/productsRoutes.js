a1fb3aefaae464f8786aa38536e4dfa665a5a893
Joao Lima <jmflima@gmail.com>
2023-01-27 17:53:45 -0300

    6-editando dado get

---
+++ 13_MONGOOSE/6_editando_dado_get/routes/productsRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+
+const ProductController = require('../controllers/ProductController')
+
+router.get('/edit/:id', ProductController.editProduct)
+//router.post('/edit', ProductController.editProductPost)
+
+//acessa o formulário de inclusão
+router.get('/create', ProductController.createProduct)
+//grava as informações digitadas
+router.post('/create', ProductController.createProductPost)
+
+//pesquisa e exclui
+// router.post('/remove/:id', ProductController.removeProduct)
+
+//pesquisa um produto
+router.get('/:id', ProductController.getProduct)
+
+ //acessa a home
+ router.get('/', ProductController.showProducts)
+
+module.exports = router
