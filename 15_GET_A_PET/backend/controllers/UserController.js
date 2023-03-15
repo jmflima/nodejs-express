532be1d1c9145e7780853789d586730309e5706f
Joao Lima <jmflima@gmail.com>
2023-03-15 17:00:45 -0300

    User - verificação de token jwt

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -147,4 +147,12 @@ module.exports = class UserController {
         res.status(200).json({ user })
 
     }
+
+    static async editUser(req, res){
+        res.status(200).json({ 
+            message: 'Deu tudo certo' 
+        })
+        return 
+
+    }
 }
