cf3d2ec04b8d034a9a3ada2f5a7734056af9b68f
Joao Lima <jmflima@gmail.com>
2024-03-27 16:37:48 -0300

    Pet - 007 - Alterando e fazendo adoção

--- 15_GET_A_PET/backend/controllers/PetController.js
+++ 15_GET_A_PET/backend/controllers/PetController.js
@@ -130,7 +130,6 @@ module.exports = class PetController {
         if(!pet){
             res.status(404).json({ message: "Pet não encontrado"}) 
             return
-
         }
 
         res.status(201).json({ pet: pet })
@@ -148,7 +147,6 @@ module.exports = class PetController {
         if(!pet){
             res.status(404).json({ message: "Pet não encontrado"}) 
             return
-
         }
 
         //checar se o foi o usuário logado quem cadastrou o Pet
@@ -159,10 +157,154 @@ module.exports = class PetController {
         if(pet.user._id.toString() !== user._id.toString()){
             res.status(422).json({ message: "Houve um problema com sua informação. Tente mais tarde" })
             return
+
         }
 
         await Pet.findByIdAndRemove(id)
 
         res.status(200).json({ massage: "Pet removido com sucesso" })
+    }
+
+    static async updatePet(req, res) {
+
+        const id = req.params.id
+
+        const {nome, idade, peso, raca, cor, available } = req.body
+
+        const images = req.files
+
+        const updatData = []
+
+        const pet = await Pet.findOne({_id: id.toString()})
+        if(!pet){
+            res.status(404).json({ message: "Pet não encontrado"}) 
+            return
+        }
+        // testando se o pet pertense ao usuário logado
+        const token = getToken(req)
+        const user =  await getUserByToken(token)
+//        console.log(user._id)
+//        console.log(token)
+
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(422).json({ message: "Houve um problema com sua informação. Tente mais tarde" })
+            return
+
+        }
+
+        if(!nome) {
+            res.status(422).json({ message: "O nome é obrigatório" })
+            return
+        }else{
+            updatData.nome = nome
+        }
+        if(!idade) {
+            res.status(422).json({ message: "A idade é obrigatória" })
+            return
+        }else{
+            updatData.idade = idade
+        }
+        if(!peso) {
+            res.status(422).json({ message: "O peso é obrigatório" })
+            return
+        }else{
+            updatData.peso = peso
+        }
+        if(!raca) {
+            res.status(422).json({ message: "A raca é obrigatória" })
+            return
+        }else{
+            updatData.raca = raca
+        }
+        if(!cor) {
+            res.status(422).json({ message: "A cor é obrigatória" })
+            return
+        }else{
+            updatData.cor = cor
+        }
+
+        if(images.length === 0) {
+            res.status(422).json({ message: "A imagem é obrigatória" })
+            return
+        }else{
+            updatData.images = []
+            images.map((image) => {
+              updatData.images.push(image.filename)
+            })
+        }
+        await Pet.findByIdAndUpdate(id, updatData)
+//        console.log(id, updatData)
+
+        res.status(200).json({ message: "Pet atualizado com sucesso!" })
+
+    }
+
+    static async schedule(req, res) {
+      const id = req.params.id
+
+      const pet = await Pet.findOne({_id: id.toString()})
+      if(!pet){
+          res.status(404).json({ message: "Pet não encontrado"}) 
+          return
+      }
+    
+        const token = getToken(req)
+        const user =  await getUserByToken(token)
+//        console.log(user._id)
+//        console.log(token)
+
+        if(pet.user._id.equals(user._id)){
+            res.status(422)
+            .json({ message: "Agendar visita pra outro Pet" })
+            return
+        }
+
+        //checa se usuário já agendou uma visita pra esse pet
+
+        if(pet.adopter){
+            if(pet.adopter._id.equals(user._id)){
+                res.status(422)
+                .json({ message: "Você ja agendou visita para esse Pet" })
+                return
+            }
+        }
+
+        //cadastra visita de adoção
+        pet.adopter = {
+            _id: user._id,
+            name: user.name,
+            image: user.image
+        }
+      await Pet.findByIdAndUpdate(id, pet)
+
+      res.status(200).json({
+        messasge: `Visita agendada com sucesso.Entrar em contato com ${pet.user.name}, fone ${pet.user.phone}.`
+        })
+
+    }
+
+    static async concluiAdocao(req, res){
+        const id = req.params.id
+
+        const pet = await Pet.findOne({_id: id})
+        if(!pet){
+            res.status(404).json({ message: "Pet não encontrado"}) 
+            return
+        }
+        const token = getToken(req)
+        const user =  await getUserByToken(token)
+        if(user._id.toString() !== pet.adopter._id.toString()){
+            res.status(422)
+            .json({ message: "É preciso agendar para adoção" })
+            return
+        }
+
+        pet.available = false
+        await Pet.findByIdAndUpdate(id, pet)
+
+        res.status(200).json({
+            message: "Parabéns, adoção realizada com suceso"
+        })
+
     }
 }
--- 15_GET_A_PET/backend/routes/PetRoutes.js
+++ 15_GET_A_PET/backend/routes/PetRoutes.js
@@ -17,5 +17,13 @@ router.get("/mypets", verifyToken, PetController.getAllUserPets)
 router.get("/myadoptions", verifyToken, PetController.getAllUserAdoptions)
 router.get("/:id", PetController.getPetById)
 router.delete("/:id", verifyToken, PetController.removePetById)
+router.patch(
+    "/:id", 
+    verifyToken, 
+    imageUpload.array('images'), 
+    PetController.updatePet
+)
+router.patch("/schedule/:id", verifyToken, PetController.schedule)
+router.patch("/conclui/:id", verifyToken, PetController.concluiAdocao)
 
 module.exports = router
