ee1f78f1079e28ac0c0a207612f1eef9f42d52dd
Joao Lima <jmflima@gmail.com>
2024-05-07 16:08:13 -0300

    015-270 - Estruturando navbar e footer

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from "react"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
+/*componentes*/
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+
 /* pges*/
 import Home from "./components/pages/Home"
 import Login from "./components/pages/Auth/Login"
@@ -9,11 +13,13 @@ import Register from "./components/pages/Auth/Register"
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" exact element={<Home />} />
      </Routes>
+     <Footer />
     </Router>
 
   );
